8202d8fe6a53e203d111b8a614f861cc549a2cc8
kalimuthusowndappan <kalimuthu235@gmail.com>
2019-10-07 16:46:02 +0100

    Added receipes for remaining tasks

--- test.rb
+++ test.rb
@@ -12,3 +12,21 @@ file '/etc/motd'
   mode '0644'
   content 'Hello world'
 end
+
+user 'Kalimuthu.Sowndapan' do
+  comment 'DevOps Interview'
+  shell '/bin/bash'
+  password 'password'
+  action :create
+end
+
+cron 'test' do
+  hour '17'
+  minute '45'
+  command 'test'
+end
+
+timezone 'london' do
+  timezone 'Europe/London'
+  action :set
+end
